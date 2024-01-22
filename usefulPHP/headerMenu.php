2fa6d89dbf9318f3a32417f9a215e15baba9241d
fZitny <145013716+fZitny@users.noreply.github.com>
2024-01-22 09:23:55 +0100

    klolloko

--- usefulPHP/headerMenu.php
+++ usefulPHP/headerMenu.php
@@ -23,19 +23,6 @@
                 <form action=''>
                     <!--  <button type='submit'><i class='fa fa-search'></i></button> -->   
                     <input id='findInput' type='text' placeholder='Hledat ..🤡' autocomplete='off'>
-        <a class="login" href="login.php">Přihlásit</a>
-        
-
-        <div class="navbar">
-            <a href="index.php">Home</a>
-            <a href="about.php">About</a>
-            <a href="contact.php">Contact</a>
-            <a href="PridatOtazku.php">Add question</a>
-
-            <div class="vyhledavani">
-                <form action="">
-                    <!--  <button type="submit"><i class="fa fa-search"></i></button> -->   
-                    <input id="findInput" type="text" placeholder="Hledat ..🤡" autocomplete="off">
                 </form>
             </div>
         </div>
