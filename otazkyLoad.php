fb9a987520aadf75bfb709dfbb5e4650dfe125d4
fZitny <145013716+fZitny@users.noreply.github.com>
2024-01-28 01:10:10 +0100

    bing chilling

--- otazkyLoad.php
+++ otazkyLoad.php
@@ -20,10 +20,14 @@ $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-
+                echo "<div class='otazka'>";
+                $filename = "/Wordy/{$row["Subject"]}{$row["ID"]}.pdf";
                 echo"<h2 id='". $row['Nazev']."'>". $row["Subject"] . $row["ID"] ." - ". $row["Nazev"]." </h2>";
                 echo "<p>". $row["Text"] ."</p>";
-                echo "<a class='downloadText' href='/Wordy/". $row["Subject"] . $row["ID"] .".pdf'download><span class='material-symbols-outlined'>download</span>Stáhnout ". $row["Subject"] . $row["ID"] ."</a>";
+                if (true) {
+                    echo "<a class='downloadText' href='$filename'download><span class='material-symbols-outlined'>download</span>Stáhnout ". $row["Subject"] . $row["ID"] ."</a>";
+                }
+                echo "</div>";
             }
         }
 
